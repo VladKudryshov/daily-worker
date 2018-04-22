08396338c5bc985c3ac80f0c9e999464678e32be
VladKudryshov <kudryshov_vv_15@mail.ru>
2018-04-22 13:57:27 +0300

    fix

--- src/main/java/DialyWorker.java
+++ src/main/java/DialyWorker.java
@@ -1,11 +1,48 @@
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.*;
+
+import java.io.FileInputStream;
+
 public class DialyWorker {
-    public static void main(String[] args) {
+    private static DatabaseReference database;
+
+    public static void main(String[] args) throws Exception{
+
+        FileInputStream serviceAccount =
+                new FileInputStream("path/to/serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://shooplists.firebaseio.com")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+        database = FirebaseDatabase.getInstance().getReference();
         while (true) {
             try {
                 Thread.sleep(1000*60);
             } catch (InterruptedException e) {
             }
-            System.out.println("Worker process woke up");
+            getUsers();
         }
+    }
+
+    private static void getUsers(){
+        database.child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot data: dataSnapshot.getChildren()){
+                    System.out.println(data.toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
